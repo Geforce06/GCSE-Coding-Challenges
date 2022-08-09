be005d04ebbdfce306f46958be583c2b66282ffe
Geforce <george.ed.denney@gmail.com>
2022-08-09 08:45:06 +0000

    class removal
    - replaced password class replaced for a structure
    - password generating function created and being worked on

--- Coding Problems/Password Reset Program/Main.cpp
+++ Coding Problems/Password Reset Program/Main.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
 using namespace std;
 
-class password {
+struct {
   public:
     string cpassword = "123"; //current password
     string npassword; //new password
-};
+} password;
+void passwordgenerate() {
+  char myarr[] = {'T','d','F'};
+  for (int i=0; i<3; i++) {
+    cout <<"\n"<<myarr[i]<<" ";
+  }
+}
 int main() {
   string temppass; //temp variable to compare variables
-  password Obj1;
   cout << "Please enter in the current password: ";
+  passwordgenerate();
   cin >> temppass;
   
-  int comp = Obj1.cpassword.compare(temppass);  
+  int comp = password.cpassword.compare(temppass);  
   if (comp == 0) {
-    cout << "Enter new password\n";
-    cin >> Obj1.npassword;
-    if (Obj1.npassword.length() >= 8) {
-		//TODO: put magical forest code here
-      cout << "Your new password is: "<<Obj1.npassword<<"\n";
-	  cout << "-Password Statistics-\nCharacters: "<<Obj1.npassword.length()<<"\nUppercase Letters: ";
-    } else if(Obj1.npassword.length() < 8) {
+    cout << "Enter new password or enter 2 for a generated password: \n";
+    cin >> password.npassword;
+    if (password.npassword.length() >= 8) {
+      cout << "Your new password is: "<<password.npassword<<"\n";
+	  cout << "-Password Statistics-\nCharacters: "<<password.npassword.length()<<"\nUppercase Letters: ";
+    } else if(password.npassword.length() < 8) {
       cout << "Password is less than 8 characters long.\n";
     } 
   } else {
